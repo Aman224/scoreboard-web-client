b54c9b5453d57363b7aaac9c815d138eb136d7e0
Amruth Chand <amruchand.t@gmail.com>
2018-01-22 15:08:47 +0530

    Update API URI to upstream server

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-let API_URL = "http://localhost:5000/api/";
+let API_URL = "http://mec-scoreboard.herokuapp.com/api/";
 let API_REFRESH_INTERVAL = 4000
 let MODULE_SWITCH_INTERVAL = 24000
 let CLOCK_UPDATE_INTERVAL = 30000
