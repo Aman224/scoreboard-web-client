bff1e04bbc4cfe64bcf1b9105a8914a64823de18
Amruth Chand <amruchand.t@gmail.com>
2018-01-22 10:44:33 +0530

    Add table updation code

--- js/module.ongoing.js
+++ js/module.ongoing.js
@@ -1,6 +1,24 @@
 moduleOngoing = {};
 
 $(document).ready(function() {
+    let tableTemplate = [
+        '<table class="module-ongoing-entry-results-table">',
+        '    <thead>',
+        '       <tr>',
+        '           <th width="10%">#</th>',
+        '           <th>Name</th>',
+        '           <th width="25%">House</th>',
+        '       </tr>',
+        '    </thead>',
+        '    <tbody class="results-first">',
+        '    </tbody>',
+        '    <tbody class="results-second">',
+        '    </tbody>',
+        '    <tbody class="results-third">',
+        '    </tbody>',
+        '</table>'
+    ].join('\n');
+
     let entryTemplate = [
         '<div class="module-ongoing-entry-wrapper">',
         '   <div class="module-ongoing-entry-title-wrapper">',
@@ -16,11 +34,23 @@ $(document).ready(function() {
         '       <div class="module-ongoing-entry-results-title-wrapper">',
         '           <span class="module-ongoing-entry-results-title-text"></span>',
         '       </div>',
+        '       <div class="module-ongoing-entry-results-table-wrapper">',
+                    tableTemplate,
+        '       </div>',
         '   </div>',
         '</div>'
     ].join('\n');
 
+    let resultEntryTemplate = [
+        '<tr>',
+        '   <td class="td-pos"></td>',
+        '   <td class="td-name"></td>',
+        '   <td class="td-house"></td>',
+        '</tr>'
+    ].join('\n');
+
     var stageList = [];
+    var resultList = [];
 
     function formEventString (stage) {
         var eventString = '';
@@ -37,18 +67,63 @@ $(document).ready(function() {
         $(entry).find(".module-ongoing-entry-title-text")[0].innerText = '[ Stage ' + (index + 1) + ' ]';
         $(entry).find(".module-ongoing-entry-name-text")[0].innerText = stage['name'];
         $(entry).find(".module-ongoing-entry-event-text")[0].innerHTML = formEventString(stage);
-        // $(entry).find(".module-ongoing-entry-results-title-text")[0].innerHTML = 'Results, ' + 'Some event';
 
         $(entry).css("width", (100 / listSize).toString() + "%");
         $(entry).css("left", (index * (100 / listSize)).toString() + "%");
 
         $("#module-ongoing-wrapper").append(entry);
+        renderResult($(entry).find(".module-ongoing-entry-results-table-wrapper")[0], stage['id']);
     }
 
     function updateStage(stage, index, listSize) {
         var entries = $("#module-ongoing-wrapper").children(".module-ongoing-entry-wrapper");
 
         $(entries[index]).find(".module-ongoing-entry-name-text")[0].innerText = stage['name'];
+        $(entries[index]).find(".module-ongoing-entry-event-text")[0].innerHTML = formEventString(stage);
+
+        renderResult($(entries[index]).find(".module-ongoing-entry-results-table-wrapper")[0], stage['id']);
+    }
+
+    function appendResult(resultTable, result, index, listSize) {
+        var entry = $.parseHTML(resultEntryTemplate);
+
+        var parentTBody = null;
+        if (result['position'] == 1)
+            parentTBody = $(resultTable).find(".results-first")[0];
+        else if (result['position'] == 2)
+            parentTBody = $(resultTable).find(".results-second")[0];
+        else
+            parentTBody = $(resultTable).find(".results-third")[0];
+        
+        $(entry).find(".td-pos")[0].innerText = result['position'];
+        $(entry).find(".td-name")[0].innerText = result['name'];
+        $(entry).find(".td-house")[0].innerText = result['team'];
+
+        $(parentTBody).append(entry);
+    }
+
+    function renderResult(tableWrapper, stageID) {
+        $.ajax({url: API_URL + "results/" + encodeURI(stageID), crossDomain: true, dataType: 'json', success: function(result) {
+                resultEvent = result['event'];
+                result = result['results'];
+
+                if (resultList === result) {
+                    return;
+                } else {
+                    changeFunc = appendResult;
+                    $(tableWrapper).children("table").children("tbody").children("tr").remove();
+                }
+
+                // TODO: implement updateResult function
+                console.log(resultEvent);
+                $(tableWrapper).parent().find(".module-ongoing-entry-results-title-text")[0].innerText = resultEvent;
+                result.forEach(function(elem, index) {
+                    changeFunc($(tableWrapper).find("table")[0], elem, index, result.length);
+                });
+
+                resultList = result;
+            }
+        });
     }
 
     function renderStage() {
