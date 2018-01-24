e0131e31e857a6238b6679cbb79ac0cb449483af
Amruth Chand <amruchand.t@gmail.com>
2018-01-24 12:42:02 +0530

    Update photo gallery
    This commit adds CSS animations to the photo gallery.

--- js/module.photos.js
+++ js/module.photos.js
@@ -1,39 +1,36 @@
 var modulePhotos = {};
 
 $("document").ready(function() {
+    var imgElement = document.getElementById("module-photos-img");
     var currPhoto = 0;
+
+    imgElement.addEventListener("animationend", function(event) {
+        imgElement.classList.remove("module-photos-enter");
+        imgElement.classList.remove("module-photos-exit");
+
+        event.target.src = photoList[currPhoto];
+
+        if (event.animationName === "photo-flip")
+            imgElement.classList.add("module-photos-enter");
+    });
+
+    imgElement.addEventListener("animationstart", function(event) {
+
+        parentElement = document.getElementById("module-photos-wrapper");
+        event.target.removeAttribute('width');
+        event.target.removeAttribute('height');
+
+        if ((event.target.naturalWidth / event.target.naturalHeight) > (parentElement.clientWidth / parentElement.clientHeight))
+            event.target.setAttribute('width', '100%');
+        else
+            event.target.setAttribute('height', '100%');
+    });
 
     function renderPhotos() {
         if (photoList.length === 0)
             return;
         
-        var imgElement = document.getElementById("module-photos-img");
-        
-        imgElement.classList.remove("module-photos-enter");
         imgElement.classList.add("module-photos-exit");
-
-        imgElement.addEventListener("animationend", function(event) {
-            imgElement.classList.remove("module-photos-exit");
-
-            event.target.removeEventListener("animationend");
-        });
-
-        imgElement.classList.add("module-photos-enter");
-        
-        imgElement.addEventListener("animationstart", function(event) {
-            event.target.src = photoList[currPhoto];
-
-            parentElement = document.getElementById("module-photos-wrapper");
-            event.target.removeAttribute('width');
-            event.target.removeAttribute('height');
-
-            if ((event.target.naturalWidth / event.target.naturalHeight) > (parentElement.clientWidth / parentElement.clientHeight))
-                event.target.setAttribute('width', '100%');
-            else
-                event.target.setAttribute('height', '100%');
-
-            event.target.removeEventListener("animationstart");
-        });
 
         currPhoto++;
         if (currPhoto >= photoList.length)
