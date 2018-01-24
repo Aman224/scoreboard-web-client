e8479e436852c24bb304a70e46e8f9e2a18d6b5e
Amruth Chand <amruchand.t@gmail.com>
2018-01-24 09:03:56 +0530

    Update overall score handling

--- js/module.overall.js
+++ js/module.overall.js
@@ -31,7 +31,7 @@ $("document").ready(function() {
     function appendScore(score, index, listSize) {
         var entry = $.parseHTML(entryTemplate);
 
-        $(entry).find(".module-overall-entry-score-text")[0].innerText = parseInt(score['score']) + parseInt(score['other_score']);
+        $(entry).find(".module-overall-entry-score-text")[0].innerText = parseInt(score['other_score']);
         $(entry).find(".module-overall-entry-name-text")[0].innerText = score['name'];
         $(entry).find(".module-overall-entry-logo-img")[0].src = "img/" + score['logo'];
 
@@ -47,7 +47,7 @@ $("document").ready(function() {
         var entries = $("#module-overall-wrapper").children(".module-overall-entry-wrapper");
 
         $(entries[index]).find(".module-overall-entry-name-text")[0].innerText = score['name'];
-        $(entries[index]).find(".module-overall-entry-score-text")[0].innerText = parseInt(score['score']) + parseInt(score['other_score']);
+        $(entries[index]).find(".module-overall-entry-score-text")[0].innerText = parseInt(score['other_score']);
         $(entries[index]).find(".module-overall-entry-logo-img")[0].src = "img/" + score['logo'];
 
         $(entries[index]).css("background-color", rgba_convert(score['color'], 0.5));
